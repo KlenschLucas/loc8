3c84a931486677c2047d53842f9b9046fe7fd820
KlenschLucas <klensch.lucas@gmail.com>
2018-07-22 06:58:38 +0200

    Removed -

--- routes/index.js
+++ routes/index.js
@@ -54,6 +54,7 @@ router.get('/',function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   let coords = req.body.json;
+  console.log(coords)
   let policeData = require('../data/policeData');
   let hospitalData = require('../data/hospitalData');
   res.render('index', {
